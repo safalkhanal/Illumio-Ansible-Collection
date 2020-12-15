39badd29500d6bba8dc89e16af45276c472f0f71
DAVPFSN <davidnguyen0207@gmail.com>
2020-12-15 16:15:38 +1100

    Add asynchronous API call function
    2 other scripts to test the 2 API calls, just run them, to be removed later

--- plugins/modules/helpermodules/api_calls.py
+++ plugins/modules/helpermodules/api_calls.py
@@ -15,12 +15,13 @@ __email__ = "davidnguyen0207@gmail.com"
 __status__ = "In Development"
 
 import json
+import time
 import requests
 from requests.auth import HTTPBasicAuth
 
 
 # Making a synchronous API call
-# For volume under 500
+# For UNDER 500 items being queried on the server ("GET" operation)
 # Requires a credential, a http verb, resource to access (e.g. /labels for labels),
 # Does it contains the org_href or not
 # And the data to push
@@ -42,6 +43,48 @@ def sync_api(creds, http_verb, resource, has_org, payload=None):
     return response
 
 
+# Making an asynchronous API call
+# For OVER 500 items being queried on the server ("GET" operation)
+# NOTE: only apply to "GET" HTTP operation and therefore doesn't require a http verb
+# Requires a credential, resource to access (e.g. /labels for labels),
+# Does it contains the org_href or not
+# And the data to push
+def async_api(creds, resource, has_org, payload=None):
+    # Use different url depends on if the call requires an org_href
+    if has_org:
+        api_url = creds.url_with_org(resource)
+    else:
+        api_url = creds.url_with_api(resource)
 
+    # Declare headers and timeout
+    # IMPORTANT: "Prefer": "respond-async" on header
+    headers = {"Prefer": "respond-async", "Content-type": "application/json", "Accept": "application/json"}
+    timeout = 15
+
+    # Make the call
+    response = requests.request("get", api_url, auth=HTTPBasicAuth(creds.username, creds.auth_secret),
+                                headers=headers, timeout=timeout, data=json.dumps(payload))
+
+    # Since this is an asynchronous call so instead of the result,
+    # The server will send back a special URL; We will perform GET operation on that URL
+    # periodically until it's either success or fail
+    # First we wait for suggested amount of time (provided by the server)
+    time.sleep(int(response.headers['Retry-After']))
+
+    # Then we query the second URL periodically until we get a confirmation
+    status = ""
+    monitor_url = response.headers['Location']
+    while status != "done" and status != "failed":
+        response = sync_api(creds, "get", monitor_url, False)
+        status = json.loads(response.content)['status']
+        print(status)
+        time.sleep(1)
+
+    # After the status one the second URL become "done"
+    # The server will send us a third URL
+    # Doing GET operation on this URL yield results
+    response = sync_api(creds, "get", json.loads(response.content)['result']['href'], False)
+
+    return response
 
 
